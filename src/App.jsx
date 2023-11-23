a81309696b072948be6dd0bf275af91a2c7b662b
Jesus Bernal <jbbernal971220@gmail.com>
2023-11-23 12:31:38 -0500

    three commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./App.css";
-import Header from "./components/Header";
-import ModalTask from "./components/ModalTask";
+import './App.css';
+import Header from './components/Header';
+import ModalTask from './components/ModalTask';
 
 function App() {
   return (
-    <main className="bg-black min-h-screen font-['Roboto'] text-white">
+    <main className="fondo font-['Roboto'] text-white">
       <Header />
       <ModalTask />
     </main>
